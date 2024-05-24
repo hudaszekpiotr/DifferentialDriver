95771a9180fc1a0b3088de54ed1f3a830fc4e7a2
hudaszekpiotr <hudaszekpiotr@gmail.com>
2024-05-24 16:39:16 +0200

    image detection training compiles

--- ros_ws/src/image_detector_cpp/include/image_detector_cpp/train_face_recognition_cpp.hpp
+++ ros_ws/src/image_detector_cpp/include/image_detector_cpp/train_face_recognition_cpp.hpp
@@ -1,7 +1,58 @@
+#ifndef CLIFF_SENSOR_CPP_HPP_
+#define CLIFF_SENSOR_CPP_HPP_
+
 #include "interfaces/srv/label_face.hpp"
 #include <fstream>
 #include <sstream>
 #include <filesystem>
 #include <fstream>
+#include <sstream>
 
-const std::string CSV_FILENAME = "data.csv"
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/image.hpp"
+#include "std_srvs/srv/empty.hpp"
+#include "visualization_msgs/msg/marker.hpp"
+
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/objdetect.hpp>
+#include <opencv2/face.hpp>
+#include "std_msgs/msg/header.hpp"
+#include <ament_index_cpp/get_package_share_directory.hpp>
+
+const std::string CSV_FILENAME = "data.csv";
+std::string package_share_directory = ament_index_cpp::get_package_share_directory("startup");
+std::string FACE_CASCADE_FILENAME = package_share_directory + "/config/haarcascade_frontalface_default.xml";
+
+using namespace std::chrono_literals;
+
+
+class FaceTrainer : public rclcpp::Node
+{
+public:
+    FaceTrainer();
+    void new_image_callback(const sensor_msgs::msg::Image& msg);
+    void detect_faces(cv::Mat image);
+    void pause_callback(const std::shared_ptr<std_srvs::srv::Empty::Request> request, std::shared_ptr<std_srvs::srv::Empty::Response> response);
+    void resume_callback(const std::shared_ptr<std_srvs::srv::Empty::Request> request, std::shared_ptr<std_srvs::srv::Empty::Response> response);
+    void label_callback(const std::shared_ptr<interfaces::srv::LabelFace::Request> request,
+          std::shared_ptr<interfaces::srv::LabelFace::Response> response);
+    void train_callback(const std::shared_ptr<std_srvs::srv::Empty::Request> request, std::shared_ptr<std_srvs::srv::Empty::Response> response);
+    void generate_csv_file();
+private:
+    bool pause = false;
+    cv::Mat image;
+    std::vector<cv::Rect> faces;
+    cv::CascadeClassifier face_cascade;
+    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
+    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr detected_faces_publisher;
+    std::string recognize_face(cv::Mat face_image);
+};
+
+#endif 
--- ros_ws/src/image_detector_cpp/src/train_face_recognition_cpp.cpp
+++ ros_ws/src/image_detector_cpp/src/train_face_recognition_cpp.cpp
@@ -1,22 +1,45 @@
 #include "image_detector_cpp/train_face_recognition_cpp.hpp"
 
 using namespace std::chrono_literals;
+using namespace std::placeholders;
+
+bool read_csv(const std::string& filename, std::vector<cv::Mat>& images, std::vector<int>& labels, char separator = ';') {
+    std::ifstream file(filename.c_str(), std::ifstream::in);
+    if (!file) {
+        return false;
+    }
+    std::string line, path, classlabel;
+    while (getline(file, line)) {
+        std::stringstream liness(line);
+        getline(liness, path, separator);
+        getline(liness, classlabel);
+        if(!path.empty() && !classlabel.empty()) {
+            images.push_back(cv::imread(path, 0));
+            labels.push_back(atoi(classlabel.c_str()));
+        }
+    }
+    return true;
+}
+
 
 FaceTrainer::FaceTrainer() : Node("face_trainer_node") {
     subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
-        "image_raw", 10, std::bind(&ImageDetector::new_image_callback, this, std::placeholders::_1));
+        "image_raw", 10, std::bind(&FaceTrainer::new_image_callback, this, _1));
 
     detected_faces_publisher = this->create_publisher<sensor_msgs::msg::Image>("detected_faces_image", 10);
 
     if(!face_cascade.load(FACE_CASCADE_FILENAME)){
-        RCLCPP_DEBUG_STREAM(this->get_logger(), "Error: face cascade file not valid: " << FACE_CASCADE_FILENA>    }
+        RCLCPP_DEBUG_STREAM(this->get_logger(), "Error: face cascade file not valid: " << FACE_CASCADE_FILENAME);
+    }
 
     rclcpp::Service<interfaces::srv::LabelFace>::SharedPtr label_service =
-    	this->create_service<interfaces::srv::LabelFace>("label_face", this.label_callback);
+        this->create_service<interfaces::srv::LabelFace>("label_face", std::bind(&FaceTrainer::label_callback, this, _1, _2));
     rclcpp::Service<std_srvs::srv::Empty>::SharedPtr pause_service =
-        this->create_service<std_srvs::srv::Empty>("pause_face_detection", this.pause_callback);
+        this->create_service<std_srvs::srv::Empty>("pause_face_detection", std::bind(&FaceTrainer::pause_callback, this, _1, _2));
     rclcpp::Service<std_srvs::srv::Empty>::SharedPtr resume_service =
-        this->create_service<std_srvs::srv::Empty>("resume_face_detection", this.resume_callback);
+        this->create_service<std_srvs::srv::Empty>("resume_face_detection", std::bind(&FaceTrainer::resume_callback, this, _1, _2));
+    rclcpp::Service<std_srvs::srv::Empty>::SharedPtr train_service =
+        this->create_service<std_srvs::srv::Empty>("train_face_detection", std::bind(&FaceTrainer::train_callback, this, _1, _2));
 }
 
 
@@ -35,60 +58,56 @@ void FaceTrainer::detect_faces(cv::Mat image){
     cv::cvtColor( image, image_gray, cv::COLOR_BGR2GRAY );
     cv::equalizeHist( image_gray, image_gray );
 
-    std::vector<cv::Rect> faces;
     face_cascade.detectMultiScale( image_gray, faces );
 
     for(auto face: faces){
         cv::Point center( face.x + face.width/2, face.y + face.height/2 );
         cv::ellipse( image, center, cv::Size( face.width/2, face.height/2 ), 0, 0, 360, cv::Scalar( 255, 0, 255 ), 4 );
-        extracted_face = frame_gray( face );
+        cv::Mat extracted_face = image_gray(face);
 
-        std::string label = recognize_face(extracted_face);
+        std::string label = this->recognize_face(extracted_face);
         //draw label or unrecognized1
     }
     sensor_msgs::msg::Image::SharedPtr image_msg = cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", image).toImageMsg();
     detected_faces_publisher->publish(*image_msg.get());
 }
 
-void FaceTrainer::pause_callback(){
+void FaceTrainer::pause_callback(const std::shared_ptr<std_srvs::srv::Empty::Request> request, std::shared_ptr<std_srvs::srv::Empty::Response> response){
     pause = true;
 }
 
-void FaceTrainer::resume_callback(){
+void FaceTrainer::resume_callback(const std::shared_ptr<std_srvs::srv::Empty::Request> request, std::shared_ptr<std_srvs::srv::Empty::Response> response){
     pause = false;
 }
 
 void FaceTrainer::label_callback(const std::shared_ptr<interfaces::srv::LabelFace::Request> request,
           std::shared_ptr<interfaces::srv::LabelFace::Response> response)
 {
-    cv::Mat image_gray;
-    cv::cvtColor( image, image_gray, cv::COLOR_BGR2GRAY );
-    std::vector<cv::Rect> faces;
-    face_cascade.detectMultiScale( image_gray, faces );
-
     auto t = std::time(nullptr);
     auto tm = *std::localtime(&t);
     std::ostringstream filename;
     filename << "data/" << request->label << "/" << std::put_time(&tm, "%d-%m-%Y %H-%M-%S") << ".jpg";
-    imwrite(filename, faces[request->id]);
+    cv::imwrite(filename.str(), image(faces[request->id]));
 
-    response->status = "ok";
+    response->success = true;
 }
 
-void FaceTrainer::train(){
+void FaceTrainer::train_callback(const std::shared_ptr<std_srvs::srv::Empty::Request> request, std::shared_ptr<std_srvs::srv::Empty::Response> response){
+    std::vector<cv::Mat> images;
+    std::vector<int> labels;
     generate_csv_file();
 
-    try {
-        read_csv(CSV_FILENAME, images, labels);
-    } catch (const cv::Exception& e) {
-        cerr << "Error opening file \"" << fn_csv << "\". Reason: " << e.msg << endl;
+    auto success = read_csv(CSV_FILENAME, images, labels);
+    if(!success){
+        RCLCPP_ERROR_STREAM(this->get_logger(), "Error opening file " << CSV_FILENAME);
         return;
     }
-    // Quit if there are not enough images for this demo.
+
     if(images.size() <= 1) {
-        string error_message = "This demo needs at least 2 images to work. Please add more images >        CV_Error(Error::StsError, error_message);
+        RCLCPP_ERROR_STREAM(this->get_logger(), "To train face recognition you need at least 2 images");
+        return;
     }
-    Ptr<LBPHFaceRecognizer> model = LBPHFaceRecognizer::create();
+    cv::Ptr<cv::face::LBPHFaceRecognizer> model = cv::face::LBPHFaceRecognizer::create();
     model->train(images, labels);
 }
 
@@ -100,7 +119,7 @@ void FaceTrainer::generate_csv_file(){
     for(auto& directory : std::filesystem::recursive_directory_iterator("data")){
         if (directory.is_directory()){
             for(auto& file : std::filesystem::recursive_directory_iterator(directory)){
-                if(file.is_file()){
+                if(file.is_regular_file()){
                     csv_file << file.path().string() << ";" << number << "\n";
                 }
             }
@@ -109,10 +128,14 @@ void FaceTrainer::generate_csv_file(){
     csv_file.close();
 }
 
+std::string FaceTrainer::recognize_face(cv::Mat face_image){
+   return "test";
+}
+
 int main(int argc, char* argv[])
 {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<ImageDetector>());
+    rclcpp::spin(std::make_shared<FaceTrainer>());
     rclcpp::shutdown();
     return 0;
 }
